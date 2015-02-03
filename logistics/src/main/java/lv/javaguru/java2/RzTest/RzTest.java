4012f5018b858cc28abd694afdbcc2b57eec6b2c
romansz <romans.zuks@gmail.com>
2015-02-03 20:01:39 +0200

    RZ sec commit(test)

--- logistics/src/main/java/lv/javaguru/java2/RzTest/RzTest.java
+++ logistics/src/main/java/lv/javaguru/java2/RzTest/RzTest.java
@@ -4,6 +4,7 @@ public class RzTest {
 
     public static void main(String[] args){
 
-        System.out.println("Hello Git !");
+        System.out.println("for system.out.println do:");
+        System.out.println("sout + Tab");
     }
 }
