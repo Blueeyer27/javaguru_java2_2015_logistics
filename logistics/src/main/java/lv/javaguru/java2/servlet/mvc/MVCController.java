be4a1ce9e7ff2dafbfb7094cacdda89b5bbb8370
Viktors.Savonins <sdfsdyugjh>
2015-02-17 19:21:20 +0200

    MVC

---
+++ logistics/src/main/java/lv/javaguru/java2/servlet/mvc/MVCController.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java2.servlet.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface MVCController {
+
+    MVCModel processRequest(HttpServletRequest request,
+                            HttpServletResponse response);
+    
+}
