236d72a5db1dcc790176542af21090b21a3ac26a
and4rean <and4rean@inbox.lv>
2015-02-26 11:34:37 +0200

    UserRegResultControllerTest - navernoe nepravilnii variant

--- logistics/src/test/java/lv/javaguru/java2/servlet/mvc/UserRegResultControllerTest.java
+++ logistics/src/test/java/lv/javaguru/java2/servlet/mvc/UserRegResultControllerTest.java
@@ -31,32 +31,29 @@ public class UserRegResultControllerTest {
     public void shouldCreateNewUserInDB() throws DBException {
         User user = createUser("ABC", "123");
 
-        Mockito.doReturn(user).when(userDAO).getById(user.getUserId());
+//        Mockito.doReturn(user).when(userDAO).create(user);
+        Mockito.doNothing().when(userDAO).create(user);
 
         User userToDB = controller.createNewUserInDB(user);
 
         assertEquals(user == userToDB, true);
         assertNotNull(userToDB);
 
-/*
-        User userFromDB = userDAO.getById(user.getUserId());
-        assertEquals(user.getUserId(), userFromDB.getUserId());
-*/
-
-
     }
 
     private User createUser(String login, String password) {
-/*
         User userMock = Mockito.mock(User.class);
-        Mockito.doReturn(login).when(userMock).setLogin(login);
-        Mockito.doReturn(password).when(userMock).setPassword(password);
+        Mockito.doNothing().when(userMock).setLogin(login);
+        Mockito.doNothing().when(userMock).setPassword(password);
+//        Mockito.doReturn(login).when(userMock).setLogin(login);
+//        Mockito.doReturn(password).when(userMock).setPassword(password);
         return userMock;
-*/
+/*
         User user = new User();
         user.setLogin(login);
         user.setPassword(password);
         return user;
+*/
     }
 
 }
