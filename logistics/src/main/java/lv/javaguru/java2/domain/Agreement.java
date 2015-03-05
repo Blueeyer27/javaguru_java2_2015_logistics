b7db9bc9f82d7bbc1339fdf74318548909fa3ae9
and4rean <and4rean@inbox.lv>
2015-03-05 22:27:23 +0200

    hernja kakaja to :( - commented Agreement domain mapping

--- logistics/src/main/java/lv/javaguru/java2/domain/Agreement.java
+++ logistics/src/main/java/lv/javaguru/java2/domain/Agreement.java
@@ -1,11 +1,25 @@
 package lv.javaguru.java2.domain;
 
+import javax.persistence.*;
+
+//@Entity
+//@Table(name = "agreement")
 public class Agreement {
 
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @Column(name="id", columnDefinition = "int(11)")
     private long agreementId;
+
+//    @Column(name = "cargoId", columnDefinition = "int(11)")
     private long cargoId;
+
+//    @Column(name = "vehicleId", columnDefinition = "int(11)")
     private long vehicleId;
+
+//    @Column(name = "status", columnDefinition = "char(30)")
     private String status;
+
 
     public Agreement() {
 
