10c6772c832f1b0b398df91ee4b2850d65851842
rz <romans.zuks@gmail.com>
2015-03-06 02:01:27 +0200

    All unit tests for DAO Objects now works fine!

--- logistics/src/main/java/lv/javaguru/java2/database/hibernate/CargoDAOImpl.java
+++ logistics/src/main/java/lv/javaguru/java2/database/hibernate/CargoDAOImpl.java
@@ -8,6 +8,8 @@ import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -19,30 +21,63 @@ public class CargoDAOImpl extends DAOImpl<Cargo> implements CargoDAO {
     public List<Cargo> getByParameters(String vehicleType, Double weightFrom, Double weightTo,
                                        Date loadDateFrom, Date loadDateTo, Date unloadDateFrom, Date unloadDateTo) throws DBException {
         List cargos = getCurrentSession().createCriteria(Cargo.class)
-                .add(Restrictions.eq("vehicle_type", vehicleType))
+                .add(Restrictions.eq("vehicleType", vehicleType))
                 .add(Restrictions.between("weight", weightFrom, weightTo))
-                .add(Restrictions.between("load_date", loadDateFrom, loadDateTo))
-                .add(Restrictions.between("unload_date", unloadDateFrom, unloadDateTo))
+                .add(Restrictions.between("loadDate", loadDateFrom, loadDateTo))
+                .add(Restrictions.between("unloadDate", unloadDateFrom, unloadDateTo))
                 .list();
         return cargos;
     }
 
+    /*
+    PreparedStatement preparedStatement = connection.prepareStatement("select * from " + getTableName() +
+                    " where vehicle_type = ? and (weight between ? and ?) and " +
+                    "(load_date between ? and ?) and (unload_date between ? and ?);");
+     */
+
+
+
     @Override
-    public java.sql.Date utilDateToSQL(Date date) {
-        return (java.sql.Date) getCurrentSession().get(Cargo.class, date);
+    public java.sql.Date utilDateToSQL(java.util.Date date) {
+        java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+        return sqlDate;
     }
 
     @Override
-    public Date stringToDate(String incomingDate) {
-        return (Date) getCurrentSession().get(Cargo.class, incomingDate);
+    public java.util.Date stringToDate(String incomingDate) {
+        SimpleDateFormat formater = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            java.util.Date date = formater.parse(incomingDate);
+            return date;
+        } catch (ParseException ex) {
+            System.out.println("Invalid Date format in method stringToDate(). Should be dd/MM/yyyy");
+            ex.printStackTrace();
+            return null;
+        }
     }
 
     @Override
-    public Date stringToDate2(String incomingDate, int i) {
-        return (Date) getCurrentSession().get(Cargo.class, incomingDate);
+    public java.util.Date stringToDate2(String incomingDate, int i) {
+        SimpleDateFormat formater = new SimpleDateFormat();
+
+        switch (i) {
+            case 1:
+                formater = new SimpleDateFormat("dd/MM/yyyy");
+                break;
+            case 2:
+                formater = new SimpleDateFormat("yyyy-MM-dd");
+                break;
+        }
+
+        try {
+            java.util.Date date = formater.parse(incomingDate);
+            return date;
+        } catch (ParseException ex) {
+            System.out.println("Invalid or Empty Date format in class CargoDAOImpl -> method stringToDate().");
+            ex.printStackTrace();
+            return null;
+        }
     }
-
-
 
     @Override
     public Cargo getById(Long id) throws DBException {
