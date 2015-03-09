3bb703bfdd6f855922e31fd22da10faeb68c62a3
and4rean <and4rean@inbox.lv>
2015-03-10 01:36:01 +0200

    check if user login already exist when adding new User

--- logistics/src/main/java/lv/javaguru/java2/domain/User.java
+++ logistics/src/main/java/lv/javaguru/java2/domain/User.java
@@ -4,8 +4,7 @@ import lv.javaguru.java2.database.DBException;
 import lv.javaguru.java2.database.jdbc.CompanyDAOImpl;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+
 
 @Entity
 @Table(name = "user")
